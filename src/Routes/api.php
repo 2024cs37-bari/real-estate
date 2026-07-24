b0341d7dbef8cbf78af3e28d0a3ac093649a7a58
barikhan <barikhan20930@gmail.com>
2026-07-24 08:11:28 +0500

    fix: Add api.real-estate. route name prefix to prevent route cache collision

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -6,9 +6,9 @@ use Zerp\RealEstate\Http\Controllers\Api\PropertyApiController;
 use Zerp\RealEstate\Http\Controllers\Api\PropertyViewingApiController;
 
 Route::prefix('api')->middleware(['api.json'])->group(function () {
-    Route::group(['middleware' => ['auth:sanctum'], 'prefix' => 'real-estate'], function () {
+    Route::group(['middleware' => ['auth:sanctum'], 'prefix' => 'real-estate', 'as' => 'api.real-estate.'], function () {
         // Dashboard
-        Route::get('dashboard', [DashboardApiController::class, 'index']);
+        Route::get('dashboard', [DashboardApiController::class, 'index'])->name('dashboard');
 
         // Properties / Listings
         Route::apiResource('properties', PropertyApiController::class);
